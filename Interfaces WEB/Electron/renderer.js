e3c858b36f3a8736e387deb6a1cdfc23691202f0
Pierre LAURENT <pierrelaurent@numericable.fr>
2021-11-05 17:38:54 +0100

    05/11

---
+++ Interfaces WEB/Electron/renderer.js
@@ -0,0 +1,22 @@
+/* const ipc = require("electron").ipcRenderer;
+
+function B_ici_click(){
+    ipc.send('asynchronous-message', 'ping');
+}
+
+document.getElementById("B_ici").addEventListener("click", B_ici_click);
+
+function Reponse(event, arg){
+    const message = "Asynchronous message reply: ${arg}";
+    document.getElementById("B_ici").innerText = message;
+}
+
+ipc.on("asynchronous-reply", Reponse); */
+
+function Scanner(){
+    document.getElementById("scanner").style.display = "none";
+    document.getElementById("scannerAtt").style.display = "inline-block";
+    alert("scan");
+}
+document.getElementById("scanner").addEventListener("click", Scanner);
+
